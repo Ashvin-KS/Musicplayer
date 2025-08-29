bec07449cd16af7393069e5b3035922b8242c5c9
Ashvin K S <90855180+Ashvin-KS@users.noreply.github.com>
2025-08-29 18:00:27 +0530

    intial push

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+const SearchBar = ({ query, setQuery, handleSearch, loading }) => (
+  <form onSubmit={handleSearch} className="search-bar">
+    <input
+      type="text"
+      value={query}
+      onChange={e => setQuery(e.target.value)}
+      placeholder="Search YouTube..."
+      autoComplete="off"
+    />
+    <button type="submit" disabled={loading} className={loading ? 'loading' : ''} onClick={(e) => console.log('Search button clicked')}>
+      {loading ? 'Loading...' : 'Search'}
+    </button>
+  </form>
+);
+
+export default SearchBar;
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+// Barrel file for easy imports
+export { default as MusicHeader } from './MusicHeader';
+export { default as SearchBar } from './SearchBar';
+export { default as SearchResults } from './SearchResults';
+export { default as RightSidebar } from './RightSidebar';
+export { default as Playbar } from './Playbar';
+export { default as ErrorMessage } from './ErrorMessage';
+export { default as LeftSidebar } from './LeftSidebar';
