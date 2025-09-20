7bdfea86a9935aa6f3e55f477928cd089be1f2d8
Ashvin K S <90855180+Ashvin-KS@users.noreply.github.com>
2025-09-20 17:35:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -154,4 +154,4 @@ def play(video_id):
         return Response(status=500, response=f"Error fetching audio stream: {e}")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
