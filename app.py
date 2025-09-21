8afc7ee3591e1121c3433f865630cb85d7fd02d7
Ashvin <ashvinksg@gmail.com>
2025-09-21 10:22:27 +0530

    updated

--- app.py
+++ app.py
@@ -87,4 +87,4 @@ def fetch_playlist(playlist_id):
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
